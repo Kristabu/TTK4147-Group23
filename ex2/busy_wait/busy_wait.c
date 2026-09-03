1a7bda8f5b5c9e406b594493177373aed8fc2e7b
Ida Flakstad <ida.flakstad@gmail.com>
2026-09-03 09:04:04 +0200

    fixed path

--- ex2/busy_wait/busy_wait.c
+++ ex2/busy_wait/busy_wait.c
@@ -36,28 +36,3 @@ void busy_wait_times(void)
             break;
     }
 }
-
-int main(int argc, char **argv)
-{
-    if (argc != 2)
-    {
-        printf("Usage: %s clock|times\n", argv[0]);
-        return 1;
-    }
-
-    if (strcmp(argv[1], "clock") == 0)
-    {
-        busy_wait_clock((struct timespec){1, 0});
-    }
-    else if (strcmp(argv[1], "times") == 0)
-    {
-        busy_wait_times();
-    }
-    else
-    {
-        printf("Unknown option\n");
-        return 1;
-    }
-
-    return 0;
-}
