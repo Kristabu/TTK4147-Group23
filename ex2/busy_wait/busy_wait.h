41094743223dfcfd5a874776b69c03a8477caafc
Ida Flakstad <ida.flakstad@gmail.com>
2026-09-03 09:09:57 +0200

    wip error when making b

--- ex2/busy_wait/busy_wait.h
+++ ex2/busy_wait/busy_wait.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 #include <sys/times.h>
 #include "../timespec/timespec.h"
 
--- ex2/taskB.c
+++ ex2/taskB.c
@@ -1,33 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
-#include "../timespec/timespec.h"
-
-// read access latency
-int access_latency()
-{
-    int ns_max = 50;
-    int histogram[ns_max];
-    memset(histogram, 0, sizeof(int) * ns_max);
-
-    for (int i = 0; i < 10 * 1000 * 1000; i++)
-    {
-
-        // t1 = timer()
-        // t2 = timer()
-
-        int ns = // (t2 - t1) * ??
-
-            if (ns >= 0 && ns < ns_max)
-        {
-            histogram[ns]++;
-        }
-    }
-
-    for (int i = 0; i < ns_max; i++)
-    {
-        printf("%d\n", histogram[i]);
-    }
-}
+#include "timespec/timespec.h"
+#include "busy_wait/busy_wait.h"
 
 uint64_t rdtsc(void)
 {
@@ -37,8 +11,64 @@ uint64_t rdtsc(void)
     return val;
 }
 
-int main()
+int main(int argc, char **argv)
 {
+    struct tms now;
+    int ns;
+
+    int ns_max = 50;
+    int histogram[ns_max];
+    memset(histogram, 0, sizeof(int) * ns_max);
+
+    for (int i = 0; i < 10 * 1000 * 1000; i++)
+    {
+
+        if (argc != 2)
+        {
+            printf("Usage: %s rdtsc|clock_gettime|times\n", argv[0]);
+            return 1;
+        }
+
+        if (strcmp(argv[1], "rdtsc") == 0)
+        {
+            int cpu_freq_hz = 600117000;
+            uint64_t t1 = rdtsc();
+            uint64_t t2 = rdtsc();
+            ns = (t2 - t1) * 1000000000ULL / cpu_freq_hz;
+        }
+        else if (strcmp(argv[1], "clock_gettime") == 0)
+        {
+            struct timespec t1, t2;
+
+            clock_gettime(CLOCK_MONOTONIC, &t1);
+            clock_gettime(CLOCK_MONOTONIC, &t2);
+
+            struct timespec diff = timespec_sub(t2, t1);
+
+            ns = diff.tv_sec * 1000000000LL + diff.tv_nsec;
+        }
+        else if (strcmp(argv[1], "times") == 0)
+        {
+            clock_t t1 = times(&now);
+            clock_t t2 = times(&now);
+            ns = (long long)(t2 - t1) * 1000000000LL / sysconf(_SC_CLK_TCK);
+        }
+        else
+        {
+            printf("Unknown option\n");
+            return 1;
+        }
+
+        if (ns >= 0 && ns < ns_max)
+        {
+            histogram[ns]++;
+        }
+    }
+
+    for (int i = 0; i < ns_max; i++)
+    {
+        printf("%d\n", histogram[i]);
+    }
     /*
     Pipe the output from the program into gnuplot like this:
     ./program_name | gnuplot -e "set terminal pngcairo size 1280, 720;set output 'plot.png';set title 'My Plot';plot '<cat' with boxes"
